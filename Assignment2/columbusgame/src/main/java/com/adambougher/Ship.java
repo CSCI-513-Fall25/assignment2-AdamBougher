331812c4fe56d58bbffaba7b4495e857e495e739
AdamBougher <a.bougher1999@gmail.com>
2025-10-19 15:26:05 -0500

    now notifying observers when i move

--- Assignment2/columbusgame/src/main/java/com/adambougher/Ship.java
+++ Assignment2/columbusgame/src/main/java/com/adambougher/Ship.java
@@ -22,21 +22,31 @@ public class Ship extends Observable {
     public void goEast(gridState goToState){
         if (goToState == gridState.WATER)
         x += 1;
+        notifyObservers();
     }
 
     public void goWest(gridState goToState ){
         if (goToState == gridState.WATER )
         x -= 1;
+        notifyObservers();
     }
 
     public void goNorth(gridState goToState ){
         if (goToState == gridState.WATER)
         y -= 1;
+        notifyObservers();
     }
 
     public void goSouth(gridState goToState){
         if (goToState == gridState.WATER)
         y += 1;
-    } 
+        notifyObservers();
+    }
+
+    public void notifyObservers(){
+        int[] position = {x, y};
+        setChanged();
+        super.notifyObservers(position);
+    }
 
 }
