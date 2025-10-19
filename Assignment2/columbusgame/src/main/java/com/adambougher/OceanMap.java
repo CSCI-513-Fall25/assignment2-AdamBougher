6198177190988984781e1f6a362ef94259b2fe6d
AdamBougher <a.bougher1999@gmail.com>
2025-10-19 14:41:49 -0500

    ocean grid not sets islands and pirates when it is created

--- Assignment2/columbusgame/src/main/java/com/adambougher/OceanMap.java
+++ Assignment2/columbusgame/src/main/java/com/adambougher/OceanMap.java
@@ -1,13 +1,36 @@
 package com.adambougher;
 
 public class OceanMap {
-    boolean[][] oceanGrid = new boolean[10][10];
+    gameGrid oceanGrid;
     int scalingFactor = 50;
     public Ship ship = new Ship(5,5);
+    public pirateShip [] pirates;
+
+    public OceanMap(int size, int numIslands, int numPirates) {
+        oceanGrid = new gameGrid(size);
+        pirates = new pirateShip[numPirates];
+        
+        placeObjects(numIslands, gridState.ISLAND);
+        placeObjects(numPirates, gridState.PIRATE);
+    }
 
     // Return generated map
-    public boolean[][] getMap(){
+    public gameGrid getMap(){
         return oceanGrid;
+    }
+
+    public void placeObjects(int amt, gridState state){
+        int x = (int)(Math.random() * 10);
+        int y = (int)(Math.random() * 10);
+        
+        for (int i = 0; i < amt; i++) {
+            oceanGrid.placeObject(x, y, state);
+
+            while (oceanGrid.grid[x][y] == state) {
+                x = (int)(Math.random() * 10);
+                y = (int)(Math.random() * 10);
+            }
+        }
     }
 
 
