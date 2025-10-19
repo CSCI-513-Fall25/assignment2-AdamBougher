9a8bebf1fe2067f52439113c81acab4e6da873c4
AdamBougher <a.bougher1999@gmail.com>
2025-10-19 15:25:53 -0500

    pirate ship now has ID and prints whgere they have moved

--- Assignment2/columbusgame/src/main/java/com/adambougher/pirateShip.java
+++ Assignment2/columbusgame/src/main/java/com/adambougher/pirateShip.java
@@ -4,15 +4,36 @@ import java.util.Observable;
 import java.util.Observer;
 
 @SuppressWarnings("deprecation")
-public class pirateShip extends Ship  implements Observer {
+public class pirateShip implements Observer {
+    public int x;
+    public int y;
+    private  int ID;
 
-    public pirateShip(int x, int y) {
-        super(x, y);
+    public pirateShip(int x, int y, int id){
+        this.x = x;
+        this.y = y;
+        this.ID = id;
     }
 
     @Override
     public void update(Observable o, Object arg) {
-       
+        int[] shipPosition = (int[]) arg;
+        int shipX = shipPosition[0];
+        int shipY = shipPosition[1];
+
+        // Move pirate towards the ship
+        if (shipX > x) {
+            x++;
+        } else if (shipX < x) {
+            x--;
+        }
+
+        if (shipY > y) {
+            y++;
+        } else if (shipY < y) {
+            y--;
+        }
+        System.out.println("Pirate " + ID + " moved to: (" + x + ", " + y + ")");
     }
     
 }
