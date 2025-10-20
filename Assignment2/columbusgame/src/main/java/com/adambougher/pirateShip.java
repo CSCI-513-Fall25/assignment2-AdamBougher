4026b7758f378a74aacbdd76de23cb4601ef6d47
AdamBougher <a.bougher1999@gmail.com>
2025-10-19 20:00:39 -0500

    made a child of Ship for consistant movment

--- Assignment2/columbusgame/src/main/java/com/adambougher/pirateShip.java
+++ Assignment2/columbusgame/src/main/java/com/adambougher/pirateShip.java
@@ -4,15 +4,12 @@ import java.util.Observable;
 import java.util.Observer;
 
 @SuppressWarnings("deprecation")
-public class pirateShip implements Observer {
-    public int x;
-    public int y;
-    private  int ID;
+public class pirateShip extends Ship  implements Observer {
 
-    public pirateShip(int x, int y, int id){
-        this.x = x;
-        this.y = y;
-        this.ID = id;
+    public int ID;
+
+    public pirateShip(int x, int y) {
+        super(x, y);
     }
 
     @Override
@@ -21,19 +18,37 @@ public class pirateShip implements Observer {
         int shipX = shipPosition[0];
         int shipY = shipPosition[1];
 
-        // Move pirate towards the ship
-        if (shipX > x) {
-            x++;
-        } else if (shipX < x) {
-            x--;
+        if (Math.abs(shipY - y) > Math.abs(shipX - x)) {
+            if (shipY < y) {
+                moveTowardsShip(0); // Move North
+            } else if (shipY > y) {
+                moveTowardsShip(1); // Move South
+            }
+        }else{
+            if (shipX < x) {
+                moveTowardsShip(3); // Move West
+            } else if (shipX > x) {
+                moveTowardsShip(2); // Move East
+            }
         }
-
-        if (shipY > y) {
-            y++;
-        } else if (shipY < y) {
-            y--;
-        }
-        System.out.println("Pirate " + ID + " moved to: (" + x + ", " + y + ")");
     }
+
+    private void moveTowardsShip(int dir) {
+        switch(dir){
+            case 0: // North
+                goNorth(gridState.WATER);
+                break;
+            case 1: // South
+                goSouth(gridState.WATER);
+                break;
+            case 2: // East
+                goEast(gridState.WATER);
+                break;
+            case 3: // West
+                goWest(gridState.WATER);
+                break;
+        }
+    }
+
     
 }
